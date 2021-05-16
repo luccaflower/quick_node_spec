29b42b2ed0c6fb2775952c462ac9e4c2747a95cb
Lucca Kaasgaard Christiansen <1078059@ucn.dk>
2021-05-16 15:59:35 +0200

    moved test folder

---
+++ src/test/chaiSpec.spec.js
@@ -0,0 +1,10 @@
+const chaiSpec = require('../chaiSpec')
+const expect = require('chai').expect
+
+describe('chaiSpec', () => {
+    it('creates spec with name', () => {
+        const spec = chaiSpec.content("test")
+        expect(spec).to.contain('const test = require(\'./test\')')
+        expect(spec).to.contain('describe(\'test\', () => {')
+    })
+})
