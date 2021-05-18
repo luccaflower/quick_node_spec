dcecca01ec43284cb7cb6e858d3f915fd74d5b4f
Lucca Kaasgaard Christiansen <1078059@ucn.dk>
2021-05-18 22:49:59 +0200

    fixed chaiSpec test

--- test/chaiSpec.spec.js
+++ test/chaiSpec.spec.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect
 
 describe('chaiSpec', () => {
     it('creates spec with name', () => {
-        const spec = chaiSpec.content("test")
+        const spec = chaiSpec.content("test", "./")
         expect(spec).to.contain('const test = require(\'./test\')')
         expect(spec).to.contain('describe(\'test\', () => {')
     })
